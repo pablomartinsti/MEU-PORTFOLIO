8662387b825be7adb4c65938974aaa324b00e620
pablomartinsti <pablomartinsti@outlook.com>
2024-06-26 09:00:19 -0300

    commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const myObserver = new IntersectionObserver((entries) =>{
+ 
+    entries.forEach ( (entry) =>{
+
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+    })
+
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
+
+let header = document.querySelector('.header')
+let menu = document.querySelector('#menu')
+
+
+function mostraMenu (){
+
+
+    header.style.transform = "translateY(0%)";
+    header.style.width =  "80%";
+    header.style.height = "710px";
+    
+    
+}
+function escondermenu(){
+    header.style.transform = "translateY(-110%)";
+    header.style.width =  "100%";
+    
+    
+    
+    
+}
